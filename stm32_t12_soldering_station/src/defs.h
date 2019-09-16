41572bb340c2996f8c8f5f26089e169c2cb118a2
NitjsefniDrakkainen <pciechelski@gmail.com>
2019-09-16 12:49:29 +0200

    basic GPIO configuration done

--- stm32_t12_soldering_station/src/defs.h
+++ stm32_t12_soldering_station/src/defs.h
@@ -14,18 +14,37 @@
 #define USART1_TX_PIN 							GPIO_Pin_9
 #define USART1_RX_PIN 							GPIO_Pin_10
 #define USART1_PORT								GPIOA
-
 #endif
 
 /*
  * Pinout definitions
  */
+
+//ADC
 #define ADC_TIP_PIN								GPIO_Pin_1
 #define ADC_CUR_PIN								GPIO_Pin_2
 #define ADC_AMB_IN_PIN							GPIO_Pin_3
 #define ADC_AMB_HANDLE_PIN						GPIO_Pin_4
-
 #define ADC1_PORT								GPIOA
+
+//I2C
+#define I2C1_SCL								GPIO_Pin_6
+#define I2C1_SDA								GPIO_Pin_7
+#define I2C1_PORT								GPIOB
+
+//Timer channel 2 PWM output pin
+#define TIP_PWM_PIN								GPIO_Pin_3
+#define TIP_PWM_PORT							GPIOB
+
+//Digital inputs
+#define ENCODER_PIN_A							GPIO_Pin_9
+#define ENCODER_PIN_B							GPIO_Pin_8
+#define HANDLE_SLEEP_SWITCH						GPIO_Pin_12
+#define ENC_AB_SW_PORT							GPIOB
+
+#define ENCODER_PIN_C							GPIO_Pin_13
+#define ENC_C_PORT								GPIOC
+
 
 /*
  * ADC Channels definitions
@@ -36,5 +55,10 @@
 #define ADC_INTERNAL_AMBIENT_T_SENSE			ADC_Channel_3
 #define ADC_EXTERNAL_AMBIENT_T_SENSE			ADC_Channel_4
 
+/*
+ * Timers definitions
+ */
+
+#define TIP_PWM_TIMER							TIM2
 
 #endif /* __DEFS_H__ */
--- stm32_t12_soldering_station/src/periph_init.c
+++ stm32_t12_soldering_station/src/periph_init.c
@@ -54,6 +54,12 @@ void rcc_init()
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	RCC_ADCCLKConfig(RCC_PCLK2_Div6);
 
+	//I2C clock
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+
+	//TIM2 clock
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
 	//USART1 for debug
 #if DEBUG
 	RCC_APB1PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
@@ -64,16 +70,40 @@ void gpio_init()
 {
 	GPIO_InitTypeDef gpio;
 	GPIO_StructInit(&gpio);
-
+	//ADC inputs init
 	gpio.GPIO_Pin = ADC_TIP_PIN | ADC_CUR_PIN | ADC_AMB_IN_PIN | ADC_AMB_HANDLE_PIN;
 	gpio.GPIO_Mode = GPIO_Mode_AIN;
 	GPIO_Init(ADC1_PORT, &gpio);
+
+	//I2C1 pins init
+	//GPIO_StructInit(&gpio);
+	gpio.GPIO_Pin = I2C1_SCL | I2C1_SDA; // SCL, SDA
+	gpio.GPIO_Mode = GPIO_Mode_AF_OD;
+	gpio.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(I2C1_PORT, &gpio);
+
+	//PWM pin init
+	gpio.GPIO_Pin = TIP_PWM_PIN;
+	gpio.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(TIP_PWM_PORT, &gpio);
+
+	//Digital inputs pins init
+	gpio.GPIO_Pin = ENCODER_PIN_A | ENCODER_PIN_B | HANDLE_SLEEP_SWITCH;
+	gpio.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	gpio.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(ENC_AB_SW_PORT, &gpio);
+
+	gpio.GPIO_Pin = ENCODER_PIN_C;
+	gpio.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	gpio.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(ENC_C_PORT, &gpio);
 
 #if DEBUG
 	//UASRT1 TX
 	gpio.GPIO_Pin = USART1_TX_PIN;
 	gpio.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_Init(GPIOA, &gpio);
+	GPIO_Init(USART1_PORT, &gpio);
 
 	//UASRT1 RX
 	gpio.GPIO_Pin = USART1_RX_PIN;
